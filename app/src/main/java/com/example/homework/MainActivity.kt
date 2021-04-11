4a24c514d31adfef3c32a913e3c4ee64fbfae49c
VasilevichDev <82218030+VasilevichDev@users.noreply.github.com>
2021-04-11 20:53:49 +0300

    button1

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.homework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun onCLickGoToPicture(view: View) {
+        val intent = Intent(this, PictureActivity::class.java)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/homework/PictureActivity.kt
@@ -0,0 +1,27 @@
+package com.example.homework
+
+import android.icu.number.NumberFormatter.with
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
+class PictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture)
+
+        val url = findViewById<EditText>(R.id.editText)
+        val textUrl = url.text.toString()
+        val picture = findViewById<ImageView>(R.id.imageView)
+        val button = findViewById<Button>(R.id.Button)
+
+        button.setOnClickListener() {
+            Picasso.get().load(textUrl).into(picture)
+        }
+
+    }
+
+}
