3ec4560a0a1f70d43a8dec40f6437dbbc8e5782c
VasilevichDev <82218030+VasilevichDev@users.noreply.github.com>
2021-05-18 19:45:54 +0300

    dz6

---
+++ app/src/main/java/com/example/homework/DetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.homework
+
+import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition =
+            TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -10,12 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button6 = findViewById<Button>(R.id.button6)
+        button6.setOnClickListener {
+            val intent = Intent(this, PostcardActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
     fun onCLickGoToPicture(view: View) {
-        val intent = Intent(this, PictureActivity::class.java)
-        startActivity(intent)
+        val intentPicture = Intent(this, PictureActivity::class.java)
+        startActivity(intentPicture)
     }
+
 
 }
